13b80e07067381ab29c473d2d42cdbe2cc0562a6
nasonlee <nasonlee@thingnario.com>
2024-11-09 02:44:14 +0800

    support gracefully shutdown

--- api/src/main.ts
+++ api/src/main.ts
@@ -5,6 +5,15 @@ const port = 8080;
 
 app.use(errorHandler);
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`Server running on port ${port}`);
+});
+
+// Graceful shutdown
+process.on("SIGINT", () => {
+  console.log("Received SIGINT. Shutting down gracefully...");
+  server.close(() => {
+    console.log("Closed out remaining connections.");
+    process.exit(0);
+  });
 });
